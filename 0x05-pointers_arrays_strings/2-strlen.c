333ce4ef0f0708312f5dc1ae34b9c0787df3c4f8
Nkamissah <atok948@gmail.com>
2021-06-28 08:31:22 -0700

    push 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: value to be checked
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*(s + l))
+		l++;
+	return (l);
+}
