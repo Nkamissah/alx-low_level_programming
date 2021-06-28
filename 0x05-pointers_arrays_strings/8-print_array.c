d0e22e3456ae1d9b7fe664db658c2531f98240f4
Nkamissah <atok948@gmail.com>
2021-06-28 10:30:58 -0700

    push 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,28 +1,22 @@
-/*
- * File: 7-puts_half.c
- * Auth: Nicholas Amissah
- */
-
+#include <stdio.h>
 #include "holberton.h"
-
 /**
- * puts_half - Prints half of a string.
- * @str: The string to be printed.
+ * print_array - Print an array up to n.
+ * @a: the array of elements
+ * @n: the number of elements of the array *a.
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+	while (i < n)
 	{
-		if (i < n - 1)
-		{
+		if (i != n - 1)
 			printf("%d, ", a[i]);
-		}
 		else
-		{
 			printf("%d", a[i]);
-		}
+		i++;
 	}
 	putchar('\n');
 }
