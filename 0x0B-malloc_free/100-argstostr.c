306f70a56818d908d556a4f78ec7603e01a4f82c
Nkamissah <atok948@gmail.com>
2021-07-13 08:08:41 -0700

    push 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,84 +2,32 @@
 #include <stdlib.h>
 
 /**
- * copychars - copies chars to buffer
- * @b: destination buffer
- * @start: starting char pointer
- * @stop: ending char pointer
- */
-void copychars(char *b, char *start, char *stop)
-{
-	while (start <= stop)
-		*b++ = *start++;
-	*b = 0;
-}
-
-/**
- * wordcount - counts the number of words
- * @str: the sentence string
+ * argstostr - concatenates all arguments to one string
+ * @ac: the argument count
+ * @av: the argument array
  *
- * Return: int number of words
+ * Return: pointer to concatenated string
  */
-int wordcount(char *str)
+char *argstostr(int ac, char **av)
 {
-	int words = 0, in_word = 0;
+	int i, j, k = 0, l = 0;
+	char *ret;
 
-	while (1)
-	{
-		if (*str == ' ' || !*str)
-		{
-			if (in_word)
-				words++;
-			in_word = 0;
-			if (!*str)
-				break;
-		}
-		else
-			in_word++;
-		str++;
-	}
-	return (words);
-}
-
-/**
- * strtow - splits sentence into words
- * @str: the sentence string
- *
- * Return: pointer to string array
- */
-char **strtow(char *str)
-{
-	int words = 0, in_word = 0;
-	char **ret, *word_start;
-
-	if (!str || !*str || !wordcount(str))
+	if (ac < 1 || !av)
 		return (NULL);
-	ret = malloc(sizeof(char *) * (wordcount(str) + 1));
-	while (1)
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			l++;
+	l += ac + 1;
+	ret = malloc(sizeof(char) * l);
+	if (!ret)
+		return (NULL);
+	for (i = 0; i < ac; i++)
 	{
-		if (*str == ' ' || !*str)
-		{
-			if (in_word)
-			{
-				ret[words] = malloc(sizeof(char) * (in_word + 1));
-				if (!ret[words])
-				{
-					return (NULL);
-				}
-				copychars(ret[words], word_start, str - 1);
-				words++;
-				in_word = 0;
-			}
-			if (!*str)
-				break;
-		}
-		else
-		{
-			if (!in_word++)
-				word_start = str;
-		}
-		str++;
+		for (j = 0; av[i][j]; j++)
+			ret[k++] = av[i][j];
+		ret[k++] = '\n';
 	}
-	ret[words] = 0;
+	ret[k] = '\0';
 	return (ret);
 }
