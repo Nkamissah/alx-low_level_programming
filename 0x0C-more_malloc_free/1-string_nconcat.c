87e10eab2cbd5accee03ddeb83240b0f24cc6a94
Nkamissah <atok948@gmail.com>
2021-07-15 09:31:09 -0700

    push 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,41 @@
-#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * string_nconcat - concatenate two strings
- * @s1: the first string
- * @s2: the second string
- * @n: the maximum number of bytes to use from s2
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
  *
- * Description: Copy s1 followed by the first n bytes of s2 into dynamically
- * allocated memory, and append a terminating null-byte. If n is greater than
- * or equal to the length of the string s2, use only the bytes from s2 up to
- * it's terminating null-byte. Treat NULL as an an empty string.
- *
- * Return: If memory allocation fails, return NULL. Otherwise, return a pointer
- * to the first character of the new string.
+ * Return: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *cat;
-	unsigned int s1_len = 0;
-	unsigned int s2_len = 0;
+	unsigned int i, j, k;
+	char *s;
 
-	if (s1 != NULL)
+	if (s1 == NULL)
+		i = 0;
+	else
 	{
-		while (s1[s1_len])
-			++s1_len;
+		for (i = 0; s1[i]; i++)
+			;
 	}
-
-	if (s2 != NULL)
+	if (s2 == NULL)
+		j = 0;
+	else
 	{
-		while (s2_len < n && s2[s2_len])
-			++s2_len;
+		for (j = 0; s2[j]; j++)
+			;
 	}
-
-	cat = malloc(sizeof(char) * s1_len + s2_len + 1);
-	if (cat == NULL)
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
 		return (NULL);
-
-	for (n = 0; n < s1_len; ++n, ++cat)
-		*cat = s1[n];
-
-	for (n = 0; n < s2_len; ++n, ++cat)
-		*cat = s2[n];
-
-	*cat = '\0';
-
-	return (cat - s1_len - s2_len);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
