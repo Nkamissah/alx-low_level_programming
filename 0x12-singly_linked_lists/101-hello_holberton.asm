ab8034f26d189bbd7230c19ee14420ab8d8b5f60
Nkamissah <atok948@gmail.com>
2021-07-29 08:16:35 -0700

    push 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,15 @@
-	global	main
-	extern	printf
+global main
+	extern printf
 
-	section	.text
+	section .text
 main:
-	mov	rdi, format
-	mov	rax, 0
-	call	printf
+	push rbp
+	mov rdi, format
+	mov rsi, message
+	mov rax, 0
+	call printf
+	pop rbp
+	mov rax, 0
 	ret
-format:
-	db "Hello, Holberton", 10, 0
+message:	 db "Hello, Holberton", 0
+format:		db "%s", 10, 0
